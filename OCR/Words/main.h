0fb4be7052e99c393d1a90dc41fb1670651fb24b
Ilyes Ben Taarit <ilyes.bentaarit@epita.fr>
2017-03-31 12:02:59 +0200

    Final file

---
+++ OCR/Words/main.h
@@ -0,0 +1,36 @@
+# define _XOPEN_SOURCE 500
+
+# include <assert.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <err.h>
+# include <time.h>
+# include "pixel_operations.h"
+# include <SDL.h>
+# include <SDL_image.h>
+
+void wait_for_keypressed(void);
+
+void init-sdl(void);
+
+SDL_Surface* load_image(char *path);
+
+SDL_Surface* display_image(SDL_Surface *img);
+
+int pixel_test(size_t x, size_t y);
+
+size_t defineSliceHeight();
+
+void defineMargin(size_t* leftMargin, size_t* rightMargin, size_t* upMargin, size_t* lowMargin, size_t left, size_t right, size_t up, size_t down);
+
+void draw_line(size_t x1, size_t y1, size_t x2, size_t y2);
+
+int slice_test(size_t x, size_t y);
+
+void cut_chara(size_t leftMargin, size_t upMargin, size_t lowMargin, size_t len);
+
+void cut_line(size_t leftMargin, size_t rightMargin, size_t upMargin, size_t lowMargin);
+
+void cut_block(size_t left, size_t right, size_t up, size_t low);
+
+int main (int argc, char* argv[]);
