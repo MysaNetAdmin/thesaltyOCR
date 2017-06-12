1522ec555ac77cc323f98a5c1c992062ed91c257
Bullcharge <3ullcharge@gmail.com>
2017-06-12 16:55:25 +0200

    resize

--- OCR/segmentation/main.c
+++ OCR/segmentation/main.c
@@ -6,21 +6,21 @@
 
 
 struct list{
-	struct list *next;
-	void        *data;
+  struct list *next;
+  void        *data;
 };
 
 /*
  * queue container: replace sentinel and add abstraction
  */
 struct queue {
-	struct list *store;
-	size_t       size;
+  struct list *store;
+  size_t       size;
 };
 
 void queue_init(struct queue *queue){
-	queue->size = 0;
-	queue->store = NULL;
+  queue->size = 0;
+  queue->store = NULL;
 }
 
 /*
@@ -28,11 +28,11 @@ void queue_init(struct queue *queue){
  */
 int queue_is_empty(struct queue *queue) {
 
-	if (queue->size == 0) {
-		return (1);
-	} else {
-		return (0);
-	}
+  if (queue->size == 0) {
+    return (1);
+  } else {
+    return (0);
+  }
 }
 
 /*
@@ -40,15 +40,15 @@ int queue_is_empty(struct queue *queue) {
  */
 void queue_push(struct queue *queue, void *elm) {
 
-	struct list* node = malloc(sizeof (struct list));
-	node->next = (queue_is_empty(queue) ? node : queue->store->next);
-	node->data = elm;
+  struct list* node = malloc(sizeof (struct list));
+  node->next = (queue_is_empty(queue) ? node : queue->store->next);
+  node->data = elm;
 
-	if (!queue_is_empty(queue)) {
-		queue->store->next = node;
-	}
-	queue->store = node;
-	queue->size += 1;
+  if (!queue_is_empty(queue)) {
+    queue->store->next = node;
+  }
+  queue->store = node;
+  queue->size += 1;
 }
 
 
@@ -60,252 +60,252 @@ void queue_push(struct queue *queue, void *elm) {
  */
 void* queue_pop(struct queue *queue) {
 
-	if (queue_is_empty(queue)) {
-		return (NULL);
-	} else {
-		struct list* tmp = queue->store->next;
-		queue->store->next = queue->store->next->next;
-		void* elm = tmp->data;
-		//free(tmp);
-		queue->size -= 1;
-		return (elm);
-	}
+  if (queue_is_empty(queue)) {
+    return (NULL);
+  } else {
+    struct list* tmp = queue->store->next;
+    queue->store->next = queue->store->next->next;
+    void* elm = tmp->data;
+    //free(tmp);
+    queue->size -= 1;
+    return (elm);
+  }
 }
 
 
 void wait_for_keypressed(void){
-	SDL_Event             event;
-	for(;;){
-		SDL_PollEvent( &event );
-		switch (event.type){
+  SDL_Event             event;
+  for(;;){
+    SDL_PollEvent( &event );
+    switch (event.type){
 
-			case SDL_KEYDOWN: return;
-			default: break;
-		}
-	}
+      case SDL_KEYDOWN: return;
+      default: break;
+    }
+  }
 }
 
 
 SDL_Surface* init_SDL(size_t height, size_t width) {
-	SDL_Surface *img = SDL_CreateRGBSurface(0,width,height,32,0,0,0,0);
-	for(size_t i = 0; i < height; i++){
-		for(size_t j = 0; j < width; j++){
-			Uint32 pix = SDL_MapRGB(img -> format,0,0,0);
-			putpixel(img,j,i,pix);
-		}
-	}
-	return img;
+  SDL_Surface *img = SDL_CreateRGBSurface(0,width,height,32,0,0,0,0);
+  for(size_t i = 0; i < height; i++){
+    for(size_t j = 0; j < width; j++){
+      Uint32 pix = SDL_MapRGB(img -> format,0,0,0);
+      putpixel(img,j,i,pix);
+    }
+  }
+  return img;
 
 }
 
 SDL_Surface* load_image(char *path){
-	SDL_Surface           *img;
-	img = IMG_Load(path);
-	if (!img) errx(3, "can't load %s: %s", path, IMG_GetError());
-	return img;
+  SDL_Surface           *img;
+  img = IMG_Load(path);
+  if (!img) errx(3, "can't load %s: %s", path, IMG_GetError());
+  return img;
 }
 
 SDL_Surface* display_image(SDL_Surface *img) {
-	SDL_Surface             *screen;
+  SDL_Surface             *screen;
 
-	screen = SDL_SetVideoMode(img->w, img->h, 0, SDL_SWSURFACE|SDL_ANYFORMAT);
-	if(screen == NULL) {
-		errx(1, "Couldn't  set %dx%d video mode: %s\n",
-				img->w, img->h, SDL_GetError());
-	}
+  screen = SDL_SetVideoMode(img->w, img->h, 0, SDL_SWSURFACE|SDL_ANYFORMAT);
+  if(screen == NULL) {
+    errx(1, "Couldn't  set %dx%d video mode: %s\n",
+        img->w, img->h, SDL_GetError());
+  }
 
-	if(SDL_BlitSurface(img, NULL, screen, NULL) < 0)
-		warnx("BlitSurface error: %s\n", SDL_GetError());
+  if(SDL_BlitSurface(img, NULL, screen, NULL) < 0)
+    warnx("BlitSurface error: %s\n", SDL_GetError());
 
-	SDL_UpdateRect(screen, 0, 0, img->w, img->h);
-	wait_for_keypressed();
+  SDL_UpdateRect(screen, 0, 0, img->w, img->h);
+  wait_for_keypressed();
 
-	return screen;
+  return screen;
 }
 
 
 void trait_line(SDL_Surface *img)
 
 {
-	size_t width = img->w;
-	size_t height = img->h;
-	int boo = 0;
-	size_t cpt;
-	size_t frontline,backline;
-	for(size_t i = 0;i < height;i++){
-		cpt = 0;
-		for(size_t j = 0;j < width;j++){
-			Uint32 pix = getpixel(img,j,i);
-			Uint8 r,g,b;
-			SDL_GetRGB(pix, img->format, &r, &g, &b);
+  size_t width = img->w;
+  size_t height = img->h;
+  int boo = 0;
+  size_t cpt;
+  size_t frontline,backline;
+  for(size_t i = 0;i < height;i++){
+    cpt = 0;
+    for(size_t j = 0;j < width;j++){
+      Uint32 pix = getpixel(img,j,i);
+      Uint8 r,g,b;
+      SDL_GetRGB(pix, img->format, &r, &g, &b);
 
-			if(r < 122 && !boo){
-				frontline = i;
-				boo = 1;
-			}
-			else if(r < 122 && boo){
-				cpt++;
-			}
-		}
-		if(boo && cpt == 0){
-			backline = i;
-			boo = 0; 
-			for(size_t k = 0 ;k < width;k++){
-				Uint32 pix = SDL_MapRGB(img->format,0,0,0);
-				putpixel(img,k,frontline - 1,pix);
-				putpixel(img,k,backline ,pix);
-			}
+      if(r < 122 && !boo){
+        frontline = i;
+        boo = 1;
+      }
+      else if(r < 122 && boo){
+        cpt++;
+      }
+    }
+    if(boo && cpt == 0){
+      backline = i;
+      boo = 0; 
+      for(size_t k = 0 ;k < width;k++){
+        Uint32 pix = SDL_MapRGB(img->format,0,0,0);
+        putpixel(img,k,frontline - 1,pix);
+        putpixel(img,k,backline ,pix);
+      }
 
-		}
-	}
-	display_image(img);
+    }
+  }
+  display_image(img);
 }
 
 struct queue* trait_column(struct queue* queue){
-	struct queue* res = malloc(sizeof(struct queue));
-	queue_init(res);
-	while(!(queue_is_empty(queue)))
-	{
-		SDL_Surface *img = queue_pop(queue);
-		size_t width = img->w;
-		size_t height = img->h;
-		int boo = 0;
-		size_t cpt;
-		size_t frontline,backline;
-		for(size_t i = 0;i < width;i++){
-			cpt = 0;
-			for(size_t j = 0;j < height;j++){
-				Uint32 pix = getpixel(img,i,j);
-				Uint8 r,g,b;
-				SDL_GetRGB(pix, img->format, &r, &g, &b);
+  struct queue* res = malloc(sizeof(struct queue));
+  queue_init(res);
+  while(!(queue_is_empty(queue)))
+  {
+    SDL_Surface *img = queue_pop(queue);
+    size_t width = img->w;
+    size_t height = img->h;
+    int boo = 0;
+    size_t cpt;
+    size_t frontline,backline;
+    for(size_t i = 0;i < width;i++){
+      cpt = 0;
+      for(size_t j = 0;j < height;j++){
+        Uint32 pix = getpixel(img,i,j);
+        Uint8 r,g,b;
+        SDL_GetRGB(pix, img->format, &r, &g, &b);
 
-				if(r < 122 && !boo){
-					frontline = i;
-					boo = 1;
-				}
-				else if(r < 122 && boo){
-					cpt++;
-				}
-			}
-			if(boo && cpt == 0){
-				backline = i;
-				boo = 0; 
-				for(size_t k = 0 ;k < height;k++){
-					Uint32 pix = SDL_MapRGB(img->format,0,0,0);
-					putpixel(img,frontline,k,pix);
-					putpixel(img,backline,k ,pix);
-				}
-			}
-		}
-		display_image(img);
-		//queue_push(res,img);
-	}
-	return res;
+        if(r < 122 && !boo){
+          frontline = i;
+          boo = 1;
+        }
+        else if(r < 122 && boo){
+          cpt++;
+        }
+      }
+      if(boo && cpt == 0){
+        backline = i;
+        boo = 0; 
+        for(size_t k = 0 ;k < height;k++){
+          Uint32 pix = SDL_MapRGB(img->format,0,0,0);
+          putpixel(img,frontline,k,pix);
+          putpixel(img,backline,k ,pix);
+        }
+      }
+    }
+    display_image(img);
+    //queue_push(res,img);
+  }
+  return res;
 }
 struct queue* horizon(SDL_Surface *img, int print){
 
-	struct queue *queue;
-	queue = malloc(sizeof(struct queue));
-	queue->store = NULL;
-	queue->size = 0;
+  struct queue *queue;
+  queue = malloc(sizeof(struct queue));
+  queue->store = NULL;
+  queue->size = 0;
 
-	size_t width = img->w;
-	size_t height = img->h;
-	int boo = 0;
-	size_t cpt;
-	size_t frontline,backline;
+  size_t width = img->w;
+  size_t height = img->h;
+  int boo = 0;
+  size_t cpt;
+  size_t frontline,backline;
 
-	for(size_t i = 0;i < height;i++){
-		cpt = 0;
-		for(size_t j = 0;j < width;j++){
-			Uint32 pix = getpixel(img,j,i);
-			Uint8 r,g,b;
-			SDL_GetRGB(pix, img->format, &r, &g, &b);
+  for(size_t i = 0;i < height;i++){
+    cpt = 0;
+    for(size_t j = 0;j < width;j++){
+      Uint32 pix = getpixel(img,j,i);
+      Uint8 r,g,b;
+      SDL_GetRGB(pix, img->format, &r, &g, &b);
 
-			if(r < 122 && !boo){
-				frontline = i;
-				boo = 1;
-			}
-			else if(r < 122 && boo){
-				cpt++;
-			}
-		}
-		if(boo && cpt == 0){
-			backline = i;
-			boo = 0;
-			SDL_Surface* inter = SDL_CreateRGBSurface(0,width,backline - frontline,32,0,0,0,0);
-			for(size_t k = frontline; k < backline; k++)
-			{
-				for(size_t l = 0; l < width ;l++)
-				{
-					Uint32 pix = getpixel(img,l,k);
-					putpixel(inter,l,k - frontline,pix);
-				}
-			}
-			if(print)
-				display_image(inter);
-			queue_push(queue,inter);
-		}
-	}
-	return queue;
+      if(r < 122 && !boo){
+        frontline = i;
+        boo = 1;
+      }
+      else if(r < 122 && boo){
+        cpt++;
+      }
+    }
+    if(boo && cpt == 0){
+      backline = i;
+      boo = 0;
+      SDL_Surface* inter = SDL_CreateRGBSurface(0,width,backline - frontline,32,0,0,0,0);
+      for(size_t k = frontline; k < backline; k++)
+      {
+        for(size_t l = 0; l < width ;l++)
+        {
+          Uint32 pix = getpixel(img,l,k);
+          putpixel(inter,l,k - frontline,pix);
+        }
+      }
+      if(print)
+        display_image(inter);
+      queue_push(queue,inter);
+    }
+  }
+  return queue;
 }
 //same thing in vertical
 
 struct queue* vertical(struct queue *kebab)
 {
-	struct queue *queue;
-	queue = malloc(sizeof(struct queue));
-	queue->store = NULL;
-	queue->size = 0;
+  struct queue *queue;
+  queue = malloc(sizeof(struct queue));
+  queue->store = NULL;
+  queue->size = 0;
 
-	while(!queue_is_empty(kebab))
-	{
-		SDL_Surface* img = queue_pop(kebab);
-		size_t width = img->w;
-		size_t height = img->h;
-		int boo = 0;
-		size_t cpt;
-		size_t frontline,backline;
-		for(size_t i = 0;i < width;i++){
-			cpt = 0;
-			for(size_t j = 0;j < height;j++){
-				Uint32 pix = getpixel(img,i,j);
-				Uint8 r,g,b;
-				SDL_GetRGB(pix, img->format, &r, &g, &b);
+  while(!queue_is_empty(kebab))
+  {
+    SDL_Surface* img = queue_pop(kebab);
+    size_t width = img->w;
+    size_t height = img->h;
+    int boo = 0;
+    size_t cpt;
+    size_t frontline,backline;
+    for(size_t i = 0;i < width;i++){
+      cpt = 0;
+      for(size_t j = 0;j < height;j++){
+        Uint32 pix = getpixel(img,i,j);
+        Uint8 r,g,b;
+        SDL_GetRGB(pix, img->format, &r, &g, &b);
 
-				if(r < 122 && !boo){
-					frontline = i;
-					boo = 1;
-				}
-				else if(r < 122 && boo){
-					cpt++;
-				}
-			}
-			if(boo && cpt == 0){
-				backline = i;
-				boo = 0;
-				SDL_Surface* inter = SDL_CreateRGBSurface(0,backline - frontline,height,32,0,0,0,0);
-				for(size_t k = frontline; k < backline; k++)
-				{
-					for(size_t l = 0; l < height ;l++)
-					{
-						Uint32 pix = getpixel(img,k,l);
-						putpixel(inter,k - frontline,l,pix);
-					}
-				}
-				queue_push(queue,inter);
-			}
-		}
-	}
-	free(kebab);
-	return queue;
+        if(r < 122 && !boo){
+          frontline = i;
+          boo = 1;
+        }
+        else if(r < 122 && boo){
+          cpt++;
+        }
+      }
+      if(boo && cpt == 0){
+        backline = i;
+        boo = 0;
+        SDL_Surface* inter = SDL_CreateRGBSurface(0,backline - frontline,height,32,0,0,0,0);
+        for(size_t k = frontline; k < backline; k++)
+        {
+          for(size_t l = 0; l < height ;l++)
+          {
+            Uint32 pix = getpixel(img,k,l);
+            putpixel(inter,k - frontline,l,pix);
+          }
+        }
+        queue_push(queue,inter);
+      }
+    }
+  }
+  free(kebab);
+  return queue;
 }
 
 
 //put in white each part which is not text
 SDL_Surface* text(SDL_Surface *img){
 
-	return img;
+  return img;
 }
 
 
@@ -313,131 +313,106 @@ SDL_Surface* text(SDL_Surface *img){
 
 int** fill(SDL_Surface* tmp){
 
-		size_t width = tmp->w;
-		size_t height = tmp->h;
+  size_t width = tmp->w;
+  size_t height = tmp->h;
 
-		int** res = malloc(sizeof *res * 16);
-    for(size_t k = 0;k < 16;k++){
-      res[k] = malloc(sizeof **res * 16);
+  int** res = malloc(sizeof *res * 16);
+  for(size_t k = 0;k < 16;k++){
+    res[k] = malloc(sizeof **res * 16);
+  }
+
+  for(size_t i = 0;i < height;i++){
+    for(size_t j = 0;j < width;j++){
+        Uint32 pix = getpixel(tmp,j,i);
+        Uint8 r,g,b;
+        SDL_GetRGB(pix,tmp->format,&r,&g,&b);
+        if(r < 122) res[i][j] =  1;
+        else  res[i][j] = 0;
+      }
     }
-    size_t mati,matj = 0 , 0;
-		for(size_t i = 0;i < height;i++){
-			for(size_t j = 0;j < width;j++){
-				if(i < width && j < height){
-					Uint32 pix = getpixel(tmp,j,i);
-					Uint8 r,g,b;
-          SDL_GetRGB(pix,tmp->format,&r,&g,&b);
-          if(r < 122) res[mati][matj] =  1;
-          else  res[mati][matj] = 0;
-          matj++;
-				}
-				else{
-					Uint32 pix = SDL_MapRGB(inter->format,255,255,255);
-					putpixel(inter,i,j,pix);
-				}
-			}
-		}
-	}
-	return res;
+  return res;
 }
 
 int** resize(SDL_Surface* tmp){
 
-    size_t width = tmp->w;
-    size_t height = tmp->h;
+  size_t width = tmp->w;
+  size_t height = tmp->h;
+  //printf("%zu , %zu\n",width , height);
+  size_t rapw = width / 16;
+  size_t raph = height / 16;
+  int** res = malloc(sizeof *res * 16);
+  for(size_t k = 0;k < 16;k++){
+    res[k] = malloc(sizeof **res * 16);
+  }
 
-    size_t rapw = width / 16;
-    size_t raph = height / 16;
-    int boow = width < 32 && width > 16;
-    int booh = height < 32 && height > 16;
-    int** res = malloc(sizeof *res * 16);
-    for(size_t k = 0;k < 16;k++){
-      res[k] = malloc(sizeof **res * 16);
-    }
-  
-    size_t cptw = 0;
-    size_t cpth = 0;
-
-    for(size_t i = 0;i < height;i++){
-      if(cpth % raph == 0){
-        for(size_t j = 0;j < width;j++){
-          if(cptw % rapw == 0) {
-            Uint32 pix = getpixel(tmp,j,i);
-            Uint8 r,g,b;
-            SDL_GetRGB(pix,tmp->format,&r,&g,&b);
-
-            if(r < 122) res[i][j] = 1;
-            else  res[cpth][cptw] = 0;
+  for(size_t i = 0;i < height;i++){
+    if(i % raph == 0){
+      for(size_t j = 0;j < width;j++){
+        if(j % rapw == 0) {
+          //printf("%zu\n",j);
+          Uint32 pix = getpixel(tmp,j,i);
+          Uint8 r,g,b;
+          SDL_GetRGB(pix,tmp->format,&r,&g,&b);
+          printf("%d",r);
+          if(r < 122) {
+            res[i % raph][j % rapw] = 1;
           }
-          cptw++;
+          else {
+            res[i % raph][j % rapw] = 0;
+          }
+          rapw++;
         }
       }
-      cpth++;
+      raph++; 
     }
   }
-  return res;
+return res;
 }
 
 //return a double-dimension array fill with 0 and 1 (0 == white pixel and 1 == black pixel) 
 
 int** matrix (SDL_Surface* img){
 
-	size_t width = img->w;
-	size_t height = img->h;
+  size_t width = img->w;
+  size_t height = img->h;
 
-	int** res = malloc(sizeof *res *16);
-	for(size_t k = 0;k < 16;k++){
-		res[k] = malloc(sizeof **res * 16);
-	}
+  int** res;
 
-	for(size_t i = 0;i < height;i++){
-		for(size_t j = 0;j < width;j++){
-
-			Uint32 pix = getpixel(img,j,i);
-			Uint8 r,g,b;
-			SDL_GetRGB(pix,img->format,&r,&g,&b);
-
-			if(r < 122){
-				res[i][j] = 1;
-			}
-			else{
-				res[i][j] = 0;
-			}
-		}
-	}
-	return res;
+  if(width > 16 || height > 16) res = resize(img);
+  else  res = fill(img);
+  return res;
 }
 
 void print_matrix(int** matrix){
-	for(int i = 0;i < 16;i++){
-		for(int j = 0;j < 16;j++){
+  for(int i = 0;i < 16;i++){
+    for(int j = 0;j < 16;j++){
       if (matrix[i][j] == 1) printf("%c[1;31m%d ",27,matrix[i][j]);
       else printf("%c[1;32m%d ", 27,matrix[i][j]); 
-        //printf("%d ",matrix[i][j]);
-		}
-		printf("\n");
-	}
+      //printf("%d ",matrix[i][j]);
+    }
+    printf("\n");
+  }
 }
 
 SDL_Surface* black_n_white(SDL_Surface* img){
-	size_t width = img->w;
-	size_t height = img->h;
-	for(size_t i = 0; i < width; ++i){
-		for(size_t j = 0; j < height; ++j){
+  size_t width = img->w;
+  size_t height = img->h;
+  for(size_t i = 0; i < width; ++i){
+    for(size_t j = 0; j < height; ++j){
 
-			Uint32 pix = getpixel(img,i,j);
-			Uint8 r,g,b;
-			SDL_GetRGB(pix,img->format, &r, &g, &b);
-			r = 0.3 * r + 0.59 * g + 0.11 * b;
-			if (r <= 123) r = 0;
-			else r = 255;
-			g = r;
-			b = r;
-			pix = SDL_MapRGB(img -> format,r,g,b);
-			putpixel(img,i,j,pix);
-		}
-	}
-	return img;
+      Uint32 pix = getpixel(img,i,j);
+      Uint8 r,g,b;
+      SDL_GetRGB(pix,img->format, &r, &g, &b);
+      r = 0.3 * r + 0.59 * g + 0.11 * b;
+      if (r <= 123) r = 0;
+      else r = 255;
+      g = r;
+      b = r;
+      pix = SDL_MapRGB(img -> format,r,g,b);
+      putpixel(img,i,j,pix);
+    }
+  }
+  return img;
 }
 
 const char usage[] =
@@ -451,71 +426,64 @@ const char usage[] =
 
 
 int main(int argc,char *argv[]){
-	if(argc <= 1)
-		printf("you forgot your image\n");
-	else if(argc <= 2)
-		errx(1, "%s", usage);
-	else{
-		char *path = argv[1];
-		unsigned int f = strtoul(argv[2], NULL, 20);
-		SDL_Surface* ver = load_image(path);
-		display_image(ver);
-		//size_t width = ver->w;
-		//size_t height = ver->h;
-		struct queue* queue = malloc(sizeof(struct queue));
-		switch(f){
-			case 0:
-				ver = black_n_white(ver);
-				display_image(ver);
-				break;
-			case 1:
-				trait_line(black_n_white(ver));
-				break;
-			case 2:
-				queue = horizon(black_n_white(ver),1);
-				free(queue);
-				break;
-			case 3:
-				queue = trait_column(horizon(black_n_white(ver),0));
-				break;
-			case 4:
-				queue = vertical(horizon(black_n_white(ver),0));
-				while(!(queue_is_empty(queue)))
-				{
-					SDL_Surface* img = queue_pop(queue);
-					print_matrix(matrix(img));
-					printf("\n");
-				}
-      case 5:
+  if(argc <= 1)
+    printf("you forgot your image\n");
+  else if(argc <= 2)
+    errx(1, "%s", usage);
+  else{
+    char *path = argv[1];
+    unsigned int f = strtoul(argv[2], NULL, 20);
+    SDL_Surface* ver = load_image(path);
+    display_image(ver);
+    //size_t width = ver->w;
+    //size_t height = ver->h;
+    struct queue* queue = malloc(sizeof(struct queue));
+    switch(f){
+      case 0:
+        ver = black_n_white(ver);
+        display_image(ver);
+        break;
+      case 1:
+        trait_line(black_n_white(ver));
+        break;
+      case 2:
+        queue = horizon(black_n_white(ver),1);
+        free(queue);
+        break;
+      case 3:
+        queue = trait_column(horizon(black_n_white(ver),0));
+        break;
+      case 4:
         queue = vertical(horizon(black_n_white(ver),0));
         while(!(queue_is_empty(queue)))
         {
+          printf("oui\n");
           SDL_Surface* img = queue_pop(queue);
-          print_matrice(matrice(img));
+          print_matrix(matrix(img));
           printf("\n");
-        }
-		}
+        }     
+    }
 
-		/*
+    /*
 
-		//trait_line(ver);
-		struct queue *final;
-		final = fill(vertical(horizon(hor)));
-		struct queue *res;
-		res = malloc(sizeof(struct queue));
-		res->store = NULL;
-		res->size = 0;
+    //trait_line(ver);
+    struct queue *final;
+    final = fill(vertical(horizon(hor)));
+    struct queue *res;
+    res = malloc(sizeof(struct queue));
+    res->store = NULL;
+    res->size = 0;
 
-		printf("%zu\n",res->size);
-		int i = 0;
-		int** inter;
-		while(!queue_is_empty(final)){
-		inter = matrix(queue_pop(final));
-		queue_push(res, inter);
-		print_matrix(inter);
-		printf("\n");
-		i++;
-		}*/
-	}
-	return 0;
+    printf("%zu\n",res->size);
+    int i = 0;
+    int** inter;
+    while(!queue_is_empty(final)){
+    inter = matrix(queue_pop(final));
+    queue_push(res, inter);
+    print_matrix(inter);
+    printf("\n");
+    i++;
+    }*/
+  }
+  return 0;
 }
