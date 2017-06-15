d9439ff89622368f3d08ecfc056ed60bd27a417c
Samy Hussaein <samy.hussaein@epita.fr>
2017-06-15 20:46:50 +0200

    modif du code pour les 4boutons

--- OCR/interface/interface.c
+++ OCR/interface/interface.c
@@ -6,7 +6,7 @@
 GtkBuilder    *builder;
 GtkWidget     *main_window;
 GtkWidget     *image;
-GtkWidget     *open, *open2, *bin, *xor, *pWindow;
+GtkWidget     *open, *open2, *black_white, *column, *matrice, *line, *xor, *pWindow;
 const gchar *sText = "OCRLogo.png";
 
 static void chooser_dialog()
@@ -16,7 +16,7 @@ static void chooser_dialog()
 }
 
 void on_activate_entry(GtkWidget *pEntry, gpointer data)
-{ 
+{
     sText = gtk_entry_get_text(GTK_ENTRY(pEntry));
 
     gtk_label_set_text(GTK_LABEL((GtkWidget*)data), sText);
@@ -28,21 +28,21 @@ void on_copier_button(GtkWidget *pButton, gpointer data)
     GtkWidget *pTempEntry;
     GtkWidget *pTempLabel;
     GList *pList;
-    
+
     gtk_button_set_label(GTK_BUTTON(pButton), "Ouvrir l'image");
- 
+
     pList = gtk_container_get_children(GTK_CONTAINER((GtkWidget*)data));
- 
+
     pTempEntry = GTK_WIDGET(pList->data);
- 
+
     pList = g_list_next(pList);
- 
+
     pList = g_list_next(pList);
 
     pTempLabel = GTK_WIDGET(pList->data);
- 
+
     sText = gtk_entry_get_text(GTK_ENTRY(pTempEntry));
- 
+
     gtk_label_set_text(GTK_LABEL(pTempLabel), sText);
     gtk_image_set_from_file(GTK_IMAGE(image), sText);
     gtk_window_set_position(GTK_WINDOW(main_window), GTK_WIN_POS_CENTER);
@@ -54,7 +54,34 @@ void black_white()
 {
   char func[500] = "./../segmentation/main ";
   strcat(func, sText);
+  char numb[500] = " 0";
+  strcat(func, numb);
+  (void)system(func);
+}
+
+void column()
+{
+  char func[500] = "./../segmentation/main ";
+  strcat(func, sText);
+  char numb[500] = " 1";
+  strcat(func, numb);
+  (void)system(func);
+}
+
+void matrice()
+{
+  char func[500] = "./../segmentation/main ";
+  strcat(func, sText);
   char numb[500] = " 3";
+  strcat(func, numb);
+  (void)system(func);
+}
+
+void line()
+{
+  char func[500] = "./../segmentation/main ";
+  strcat(func, sText);
+  char numb[500] = " 2";
   strcat(func, numb);
   (void)system(func);
 }
@@ -67,7 +94,7 @@ void reseau()
 }
 
 int main(int argc, char *argv[])
-{ 
+{
   GtkWidget *pVBox;
   GtkWidget *pEntry;
   GtkWidget *pButton;
@@ -82,14 +109,17 @@ int main(int argc, char *argv[])
   image = GTK_WIDGET(gtk_builder_get_object(builder, "image"));
   open = GTK_WIDGET(gtk_builder_get_object(builder, "open"));
   open2 = GTK_WIDGET(gtk_builder_get_object(builder, "open2"));
-  bin = GTK_WIDGET(gtk_builder_get_object(builder, "bin"));
+  black_white = GTK_WIDGET(gtk_builder_get_object(builder, "black_white"));
+  column = GTK_WIDGET(gtk_builder_get_object(builder, "column"));
+  matrice = GTK_WIDGET(gtk_builder_get_object(builder, "matrice"));
+  line = GTK_WIDGET(gtk_builder_get_object(builder, "line"));
   xor = GTK_WIDGET(gtk_builder_get_object(builder, "xor"));
   pWindow = GTK_WIDGET(gtk_builder_get_object(builder, "pWindow"));
   pVBox = GTK_WIDGET(gtk_builder_get_object(builder, "pVBox"));
-  pEntry = GTK_WIDGET(gtk_builder_get_object(builder, "pEntry")); 
+  pEntry = GTK_WIDGET(gtk_builder_get_object(builder, "pEntry"));
   pButton= GTK_WIDGET(gtk_builder_get_object(builder, "pButton"));
   pLabel = GTK_WIDGET(gtk_builder_get_object(builder, "pLabel"));
-  
+
   gtk_window_set_resizable(GTK_WINDOW(main_window), TRUE);
 
   gtk_builder_connect_signals(builder, NULL);
@@ -100,14 +130,20 @@ int main(int argc, char *argv[])
 
   g_signal_connect_swapped(xor, "clicked", G_CALLBACK(reseau), NULL);
 
-  g_signal_connect_swapped(bin, "clicked", G_CALLBACK(black_white), NULL);
+  g_signal_connect_swapped(black_white, "clicked", G_CALLBACK(black_white), NULL);
+
+  g_signal_connect_swapped(column, "clicked", G_CALLBACK(column), NULL);
+
+  g_signal_connect_swapped(matrice, "clicked", G_CALLBACK(matrice), NULL);
+
+  g_signal_connect_swapped(line, "clicked", G_CALLBACK(line), NULL);
 
   g_signal_connect(G_OBJECT(pEntry), "activate", G_CALLBACK(on_activate_entry), (GtkWidget*) pLabel);
- 
+
   g_signal_connect(G_OBJECT(pButton), "clicked", G_CALLBACK(on_copier_button), (GtkWidget*) pVBox);
- 
+
   g_object_unref(builder);
- 
+
   gtk_widget_show(main_window);
   gtk_main();
 
